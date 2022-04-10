bfca1c9ba44c724443f617168102045b76136fcb
TadasSipavicius <tasipas@gmail.com>
2022-04-10 20:57:48 +0300

    added addOrderHistory button and forms

--- src/Pages/Order.Page.tsx
+++ src/Pages/Order.Page.tsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import PageContainer from '../Layout/PageContainer';
 import {allOrders} from '../data/orders'
-import { Divider, Stack, Typography } from '@mui/material';
+import { Divider, Stack, Typography, Button, TextField } from '@mui/material';
 import OrderImage from '../Images/OrderImage.png';
 
 interface IOrder{
@@ -14,8 +14,11 @@ interface IOrder{
 
 export default function Order(){    
     const params = useParams();
-
+    
+    //const [isAddHistoryButtonClicked, setIsAddButtonClicked] = useState(false);
+    const [isAddHistoryFormIsOpen, setIsAddHistoryFormIsOpen] = useState(false);
     const [order, setOrder] = useState<IOrder>()
+
     useEffect(() => {
         allOrders.forEach(item => {
             if(item.name === params.id){
@@ -23,6 +26,10 @@ export default function Order(){
             }
         })
     }, [params.id])
+
+    const onDisplayHistoryForm = () =>{
+        setIsAddHistoryFormIsOpen(!isAddHistoryFormIsOpen);
+    }
 
     return(
         <PageContainer>
@@ -48,6 +55,20 @@ export default function Order(){
                 </Stack>
             </Stack>
             <Typography style={{borderBottom: "1px solid black", marginTop: 20}}>Užsakymo istorija:</Typography>
+            <Button style={{marginTop: 10}} variant='contained' onClick={onDisplayHistoryForm}>
+                Pakeisti Užsakymo istoriją:
+            </Button>
+            {isAddHistoryFormIsOpen ? (
+                <Stack style={{maxWidth: 400, border: "1px solid gray", padding: 20, marginTop: 10}}>
+                    <label style={{fontSize: 15, fontWeight: 500, fontStyle: "italic"}}>Būsena:</label>
+                    <TextField style={{marginBottom: 20}}/>
+                    <label style={{fontSize: 15, fontWeight: 500, fontStyle: "italic"}}>Komentaras:</label>
+                    <TextField multiline rows={5}/>
+                    <Button style={{width: 100, marginTop: 10}} variant='contained' onClick={onDisplayHistoryForm}>
+                        Pridėti
+                    </Button>
+                </Stack>
+            ) : null}
         </PageContainer>
     )
 }
