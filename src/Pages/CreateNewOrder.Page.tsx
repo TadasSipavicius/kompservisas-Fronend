f3e7c10c5390528b43d9de0d280099f3a45386e8
Tadas Sipavicius <tasipas@gmail.com>
2022-04-16 20:47:53 +0300

    Added OrderForm

--- src/Pages/CreateNewOrder.Page.tsx
+++ src/Pages/CreateNewOrder.Page.tsx
@@ -1,12 +1,85 @@
-import React from 'react';
+import { Button, Container, Divider, FormControl, InputLabel, MenuItem, Select, SelectChangeEvent, Stack, TextField, Typography } from '@mui/material';
+import React, { useState } from 'react';
 import PageContainer from '../Layout/PageContainer';
+import { availableWorkers } from '../data/availableWorkers';
+import { useNavigate } from 'react-router-dom';
 
-export default function CreateNewOrder(){
+interface IWorker {
+    workerID: number;
+    workerName: string;
+}
 
-    return(
+export default function CreateNewOrder() {
+
+    const [selectedWorker, setSelectedWorker] = useState('');
+    const navigate = useNavigate();
+
+    const handleSelectChange = (event: SelectChangeEvent) => {
+        setSelectedWorker(event.target.value as string);
+    };
+
+    const onSubmitFormClick = () => {
+        navigate('/adminpanel')
+    }
+
+    return (
         <PageContainer>
-            CREATE NEW ORDER PAGE
-            
+            <Container>
+                <Stack style={{ paddingBottom: 50 }}>
+                    <Stack>
+                        <Typography sx={{ maxWidth: 550 }} style={{ marginBottom: 2 }}>
+                            Asmens kontaktiniai duomenys:
+                        </Typography>
+                        <Divider sx={{ marginBottom: 3, }} />
+                        <TextField
+                            label="Vardas"
+                            sx={{ marginBottom: 3, maxWidth: 550 }}
+                        />
+                        <TextField
+                            label="Pavardė"
+                            sx={{ marginBottom: 3, maxWidth: 550 }}
+                        />
+                        <TextField
+                            label="E-mail'as"
+                            sx={{ marginBottom: 3, maxWidth: 550 }}
+                        />
+                        <TextField
+                            label="Telefono numeris"
+                            sx={{ marginBottom: 3, maxWidth: 550 }}
+                        />
+                    </Stack>
+                    <Stack sx={{ marginTop: 3 }}>
+                        <Typography>Informacija apie gedimą/problemą:</Typography>
+                        <Divider sx={{ marginBottom: 3 }} />
+                        <TextField
+                            label="Gedimo/Problemos aprašymas"
+                            rows={7}
+                            multiline
+                        />
+                    </Stack>
+                    <Stack sx={{ marginTop: 3 }}>
+                        <Typography>Tvarkymo informacija:</Typography>
+                        <Divider sx={{ marginBottom: 5 }} />
+                        <FormControl>
+                            <InputLabel id="laisvi-darbuotojai">Laisvi Darbuotojai</InputLabel>
+                            <Select
+                                labelId="laisvi-darbuotojai"
+                                label="Laisvi Darbuotojai"
+                                value={selectedWorker}
+                                onChange={handleSelectChange}
+                                key='worker-select'
+                            >
+                                {availableWorkers.map((worker: IWorker) => (
+                                    <MenuItem key={worker.workerID} value={worker.workerID}>{worker.workerName}</MenuItem>
+                                ))}
+                                <MenuItem value={-1}>Nėra laisvo darbuotojo</MenuItem>
+                            </Select>
+                        </FormControl>
+                    </Stack>
+                    <Divider style={{ marginTop: 45, marginBottom: 10 }} />
+                    <Button variant='contained' color='success' style={{ maxWidth: 180, height: 45 }} onClick={onSubmitFormClick}>Pateikti užsakymą</Button>
+                </Stack>
+            </Container>
         </PageContainer>
     )
 }
---
+++ src/data/availableWorkers.tsx
@@ -0,0 +1,22 @@
+export const availableWorkers = [
+    {
+        workerID: 1,
+        workerName: "Tadas Sipavicius"
+    },
+    {
+        workerID: 2,
+        workerName: "Petras Petraitis"
+    },
+    {
+        workerID: 4,
+        workerName: "Lukas Lekavicius"
+    },
+    {
+        workerID: 7,
+        workerName: "Markas Demarkas"
+    },
+    {
+        workerID: 8,
+        workerName: "Kostas Sloukas"
+    },
+]
