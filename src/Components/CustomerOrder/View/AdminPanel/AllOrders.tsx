159ae98db11457cefb84ee7af5c4104cb57d6769
Tadas Sipavicius <tasipas@gmail.com>
2022-04-24 19:10:24 +0300

    get added

--- src/Components/CustomerOrder/View/AdminPanel/AllOrders.tsx
+++ src/Components/CustomerOrder/View/AdminPanel/AllOrders.tsx
@@ -1,5 +1,6 @@
 import { Box, Grid, Typography } from '@mui/material';
-import React from 'react';
+import Axios from 'axios';
+import React, { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import {allOrders} from '../../../../data/orders';
 import '../../../../Styling/Components/OrderRow.scss'
@@ -9,7 +10,13 @@ export default function AllOrders(){
     const onOrderClick = (item: string) => (e: any) =>{
         navigate(`/order/${item}`)
     }
-    
+
+    useEffect(() =>{
+        Axios.get(`https://dapperaspnetcore20220424152102.azurewebsites.net/api/orders`).then((response: any) =>{
+            console.log(" REZAS", response);
+        });
+    }, []);
+
     return(
         <Box style={{marginTop: 15, marginBottom: 15}}>
             <Typography style={{fontSize: 15, fontWeight: 600, marginBottom: 5}}>
